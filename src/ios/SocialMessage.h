ec07c7e50c79b763cbf97133736abbd60b0d4e23
Lee Crossley <leee@hotmail.co.uk>
2013-08-24 17:56:53 +0100

    First pass of upgrade to 3.0 plugin

---
+++ src/ios/SocialMessage.h
@@ -0,0 +1,18 @@
+//
+//  SocialMessage.h
+//  Copyright (c) 2013 Lee Crossley (http://ilee.co.uk). All rights reserved.
+//
+
+#import "Foundation/Foundation.h"
+#import "MessageUI/MFMailComposeViewController.h"
+#import "Cordova/CDVPlugin.h"
+
+@interface SocialMessage : CDVPlugin <MFMailComposeViewControllerDelegate> {
+
+}
+
+- (void) send:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+- (void) sendEmailFallback:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+
+@end
---
+++ src/ios/SocialMessage.m
@@ -0,0 +1,97 @@
+//
+//  SocialMessage.m
+//  Copyright (c) 2013 Lee Crossley (http://ilee.co.uk). All rights reserved.
+//
+
+#import "SocialMessage.h"
+
+@implementation SocialMessage
+
+- (void) send:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
+{
+    NSString *message = [options objectForKey:@"message"];
+    NSArray *activityTypes = [[options objectForKey:@"activityTypes"] componentsSeparatedByString:@","];
+
+    UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:@[message] applicationActivities:Nil];
+
+    NSMutableArray *exclusions = [[NSMutableArray alloc] init];
+
+    if (![activityTypes containsObject:@"PostToFacebook"])
+    {
+        [exclusions addObject: UIActivityTypePostToFacebook];
+    }
+    if (![activityTypes containsObject:@"PostToTwitter"])
+    {
+        [exclusions addObject: UIActivityTypePostToTwitter];
+    }
+    if (![activityTypes containsObject:@"PostToWeibo"])
+    {
+        [exclusions addObject: UIActivityTypePostToWeibo];
+    }
+    if (![activityTypes containsObject:@"Message"])
+    {
+        [exclusions addObject: UIActivityTypeMessage];
+    }
+    if (![activityTypes containsObject:@"Mail"])
+    {
+        [exclusions addObject: UIActivityTypeMail];
+    }
+    if (![activityTypes containsObject:@"Print"])
+    {
+        [exclusions addObject: UIActivityTypePrint];
+    }
+    if (![activityTypes containsObject:@"CopyToPasteboard"])
+    {
+        [exclusions addObject: UIActivityTypeCopyToPasteboard];
+    }
+    if (![activityTypes containsObject:@"AssignToContact"])
+    {
+        [exclusions addObject: UIActivityTypeAssignToContact];
+    }
+    if (![activityTypes containsObject:@"SaveToCameraRoll"])
+    {
+        [exclusions addObject: UIActivityTypeSaveToCameraRoll];
+    }
+
+    activity.excludedActivityTypes = exclusions;
+
+    [self.viewController presentViewController:activity animated:YES completion:Nil];
+
+    [exclusions release];
+    [activity release];
+}
+
+- (void) sendEmailFallback:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
+{
+    NSString *message = [options objectForKey:@"message"];
+        
+    Class mailClass = (NSClassFromString(@"MFMailComposeViewController"));
+        
+    if (mailClass != nil && [mailClass canSendMail])
+    {
+        MFMailComposeViewController *composer = [[MFMailComposeViewController alloc] init];
+        composer.mailComposeDelegate = self;
+        
+        [composer setMessageBody:message isHTML:FALSE];
+        
+        [self.viewController presentModalViewController:composer animated:TRUE];
+        [composer release];
+    }
+    else
+    {
+        [self showNoMailMessage];
+    }
+}
+    
+- (void) mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error
+{
+    [self.viewController dismissModalViewControllerAnimated:TRUE];
+}
+    
+- (IBAction) showNoMailMessage
+{
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Email Configuration" message:@"You must have an email account setup on your device to use the share feature." delegate:nil cancelButtonTitle:@"Close" otherButtonTitles:nil];
+    [message show];
+}
+
+@end
