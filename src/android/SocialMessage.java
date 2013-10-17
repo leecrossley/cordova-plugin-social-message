e6d04167858e9c3bc3a36dc2e0f35dc130b0ee99
Lee Crossley <leee@hotmail.co.uk>
2013-10-17 15:38:42 +0100

    More robust if subject not supplied on android

--- src/android/SocialMessage.java
+++ src/android/SocialMessage.java
@@ -18,7 +18,11 @@ public class SocialMessage extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         JSONObject json = args.getJSONObject(0);
-        doSendIntent(json.getString("text"), json.getString("subject")); 
+        if (json.has("subject")) {
+        	doSendIntent(json.getString("text"), json.getString("subject"));
+        } else {
+        	doSendIntent(json.getString("text"), null);
+        }
         return true;
     }
 
@@ -26,7 +30,9 @@ public class SocialMessage extends CordovaPlugin {
         Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
         sendIntent.setType("text/plain");
         sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
-        sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+        if (subject != null && subject.length() > 0) {
+        	sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+        }
         this.cordova.startActivityForResult(this, sendIntent, 0);
     }
 
