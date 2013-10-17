a318bc884003728614b58a07c9e1312e1896a7dc
Lee Crossley <leee@hotmail.co.uk>
2013-10-17 15:39:21 +0100

    Hard tabs for java files

--- src/android/SocialMessage.java
+++ src/android/SocialMessage.java
@@ -15,25 +15,25 @@ import org.apache.cordova.CordovaPlugin;
 
 public class SocialMessage extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        JSONObject json = args.getJSONObject(0);
-        if (json.has("subject")) {
-        	doSendIntent(json.getString("text"), json.getString("subject"));
-        } else {
-        	doSendIntent(json.getString("text"), null);
-        }
-        return true;
-    }
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		JSONObject json = args.getJSONObject(0);
+		if (json.has("subject")) {
+			doSendIntent(json.getString("text"), json.getString("subject"));
+		} else {
+			doSendIntent(json.getString("text"), null);
+		}
+		return true;
+	}
 
-    private void doSendIntent(String text, String subject) {
-        Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
-        sendIntent.setType("text/plain");
-        sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
-        if (subject != null && subject.length() > 0) {
-        	sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
-        }
-        this.cordova.startActivityForResult(this, sendIntent, 0);
-    }
+	private void doSendIntent(String text, String subject) {
+		Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
+		sendIntent.setType("text/plain");
+		sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+		if (subject != null && subject.length() > 0) {
+			sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+		}
+		this.cordova.startActivityForResult(this, sendIntent, 0);
+	}
 
 }
