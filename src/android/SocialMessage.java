4ed9f190ee84ec15a7b5985873ce8dd9c7763d27
Loïc Favory <loic@effetb.com>
2015-04-10 13:10:59 +0200

    add facebook exception

--- src/android/SocialMessage.java
+++ src/android/SocialMessage.java
@@ -5,6 +5,7 @@
 
 package uk.co.ilee.socialmessage;
 
+import java.util.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,17 +19,41 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 import android.content.Intent;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.os.Environment;
-
+import android.os.Parcelable;
+import android.app.AlertDialog;
+import android.content.pm.PackageInfo;
+import android.content.pm.ResolveInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import org.apache.cordova.LOG;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 
 @SuppressLint("DefaultLocale")
 public class SocialMessage extends CordovaPlugin {
 
+	Context ctx;
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		ctx = this.cordova.getActivity().getApplicationContext();
+		AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
+		alertDialog.setTitle("Alert");
+		alertDialog.setMessage("Alert message to be shown");
+		alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+			new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+		alertDialog.show();
+		return true;
+		/*
 		JSONObject json = args.getJSONObject(0);
 		String text = getJSONProperty(json, "text");
 		String subject = getJSONProperty(json, "subject");
@@ -39,7 +64,7 @@ public class SocialMessage extends CordovaPlugin {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return true;
+		return true;*/
 	}
 	
 	private String getJSONProperty(JSONObject json, String property) throws JSONException {
@@ -50,6 +75,16 @@ public class SocialMessage extends CordovaPlugin {
 	}
 
 	private void doSendIntent(String text, String subject, String image, String url) throws IOException {
+		AlertDialog alertDialog = new AlertDialog.Builder(ctx).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Alert message to be shown");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+        alertDialog.show();
 		// SHARE
 	    //sharing implementation
         List<Intent> targetedShareIntents = new ArrayList<Intent>();
@@ -57,18 +92,14 @@ public class SocialMessage extends CordovaPlugin {
         sendIntent.setType("text/plain");
         String shareBody = text + " " + url;
 
-        PackageManager pm = view.getContext().getPackageManager();
-        List<ResolveInfo> activityList = pm.queryIntentActivities(sharingIntent, 0);
-
-        Intent chooserIntent = Intent.createChooser(targetedShareIntents.remove(0), "Share Etikt");
-
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetedShareIntents.toArray(new Parcelable[]{}));
-        startActivity(chooserIntent);
+        PackageManager pm = ctx.getPackageManager();
+        List<ResolveInfo> activityList = pm.queryIntentActivities(sendIntent, 0);
         // END SHARE
 
         for(final ResolveInfo app : activityList) {
 
              String packageName = app.activityInfo.packageName;
+             LOG.w("Social Message", packageName);
 
 			 if (subject != null && subject.length() > 0) {
 				sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
@@ -84,14 +115,14 @@ public class SocialMessage extends CordovaPlugin {
              		}
              		if (image != null && image.length() > 0) {
              			sendIntent.setType("image/*");
-             			final URL url = new URL(image);
+             			final URL imageUrl = new URL(image);
              			String storageDir = Environment.getExternalStorageDirectory().getPath();
              			final String path = storageDir + "/" + image.substring(image.lastIndexOf("/") + 1, image.length());
              			cordova.getThreadPool().execute(new Runnable() {
              				@Override
              				public void run() {
              					try {
-             						saveImage(url, path);
+             						saveImage(imageUrl, path);
              						sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(path)));
              						cordova.getActivity().startActivityForResult(sendIntent, 0);
              					} catch (Exception e) {
@@ -112,7 +143,7 @@ public class SocialMessage extends CordovaPlugin {
 		Intent chooserIntent = Intent.createChooser(targetedShareIntents.remove(0), "Share Etikt");
 
 		chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetedShareIntents.toArray(new Parcelable[]{}));
-		startActivity(chooserIntent);
+		ctx.startActivity(chooserIntent);
 
 	}
 	
